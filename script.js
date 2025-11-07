2b95ca2f9be2219753b77302a0761419d06e85b1
Husman399 <husman67612@gmail.com>
2025-11-07 18:30:54 +0100

    finished website tutorial

---
+++ script.js
@@ -0,0 +1,14 @@
+// function toggleMenu() {
+//     const menu = document.queryselector(".menu-links");
+//     const icon = document.queryselector(".hamburger-icon");
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+// }
+
+function toggleMenu() {
+  const menu = document.querySelector('.menu-links');
+  const icon = document.querySelector('.hamburger-icon');
+  
+  menu.classList.toggle('open');
+  icon.classList.toggle('open');
+}
